a0911d692095699a28a1f37fe7ca7798b8a99e5b
smectaz <90684643+smectaz@users.noreply.github.com>
2022-07-06 01:24:56 +0200

    install mongoose

--- app.js
+++ app.js
@@ -1,24 +1,61 @@
-const express = require('express');
+const express = require("express");
+
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(function(req, res, next) {
-    console.log('Requête reçue !');
-    next();
-})
+mongoose
+	.connect(
+		"mongodb+srv://smectaz77:Bulbytaz-77@atlascluster.yx6wq87.mongodb.net/?retryWrites=true&w=majority",
+		{ useNewUrlParser: true, useUnifiedTopology: true },
+	)
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use(function(req, res, next) {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !'});
-    next();
-});
+app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('Votre requête a été envoyer avec succés ! ');
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization",
+	);
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS",
+	);
+	next();
+});
+
+app.post("/api/stuff", (req, res, next) => {
+	console.log(req.body);
+	res.status(201).json({
+		message: "objet créé !",
+	});
+});
+
+app.get("/api/stuff", (req, res, next) => {
+	const stuff = [
+		{
+			_id: "oeihfzeoi",
+			title: "Mon premier objet",
+			description: "Les infos de mon premier objet",
+			imageUrl:
+				"https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+			price: 4900,
+			userId: "qsomihvqios",
+		},
+		{
+			_id: "oeihfzeomoihi",
+			title: "Mon deuxième objet",
+			description: "Les infos de mon deuxième objet",
+			imageUrl:
+				"https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
+			price: 2900,
+			userId: "qsomihvqios",
+		},
+	];
+	res.status(200).json(stuff);
 });
 
 module.exports = app;
