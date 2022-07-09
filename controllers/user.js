e921bedb1e24c9029c8ecb1a14083adb89ab11c0
smectaz <90684643+smectaz@users.noreply.github.com>
2022-07-10 00:04:59 +0200

    creation tokens

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -19,19 +20,23 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
             }
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
                     }
                     res.status(200).json({
                         userId: user._id,
-                        token: 'TOKEN'
+                        token: jwt.sign(
+                            { userId: user._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
                     });
                 })
-                .catch(error => res.status(500).json({ error}));
+                .catch(error => res.status(500).json({ error }));
         })
-        .catch(error => res.status(500).json({ error}));
+        .catch(error => res.status(500).json({ error }));
 };
