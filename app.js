bd94b07f9a4d010356588c7efe0ec23b786e4cee
smectaz <90684643+smectaz@users.noreply.github.com>
2022-07-09 02:43:52 +0200

    signup user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const stuffRoutes = require('./routes/stuff');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -29,5 +30,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/stuff', stuffRoutes)
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
