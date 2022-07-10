434d233bdd6cf34ba19c2a0a66c90e256d2ea6aa
smectaz <90684643+smectaz@users.noreply.github.com>
2022-07-10 18:21:13 +0200

    gestion des route avec multer

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
+
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
 
@@ -29,6 +31,7 @@ app.use((req, res, next) => {
 	next();
 });
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/stuff', stuffRoutes)
 app.use('/api/auth', userRoutes);
 
--- routes/stuff.js
+++ routes/stuff.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
+
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+//faire attention à l'ordre des routes, auth en premier pour etre sur de la personne
 
 router.get('/', auth, stuffCtrl.getAllStuff);
-router.post('/', auth, stuffCtrl.createThing);
+router.post('/', auth, multer, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
 router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
